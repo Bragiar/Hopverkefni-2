248ceb2bff27e7f3e948573e3f72136f64ca939e
Bragi Arnarson <bragi97@gmail.com>
2018-11-23 12:19:41 +0000

    Bætti content.js

--- src/lib/content.js
+++ src/lib/content.js
@@ -1,7 +1,36 @@
 import el from './helpers.js'
 
 const jsonData = '../../lectures.json';
+const contentDiv = document.querySelector('.content');
 
+
+function elementBuilder(array){
+  
+  
+  if(array.type === 'youtube'){
+    const video = contentDiv.appendChild(el('iframe')).classList.add('content__video');
+    video.setAttribute('src',`${array.data}`);
+    video.setAttribute('frameborder', '0');
+    video.setAttribute('allowfullscreen','0');
+  } else if (array.type === 'text') {
+    contentDiv.appendChild(el('p',`${array.data}`)).classList.add('content__text');
+  } else if (array.type === 'quote') {
+    contentDiv.appendChild(el('blockquote',`${array.data}`)).classList.add('content__quote').appendChild(el('cite',`${array.attribute}`));
+  } else if (array.type === 'image') {
+    const figure = contentDiv.appendChild(el('figure'));
+    figure.appendChild(el('img')).setAttribute('src',`${array.data}`);
+    figure.appendChild(el('figcaption',`${array.caption}`));
+  } else if (array.type === 'heading') {
+    contentDiv.appendChild(el('h2',`${array.data}`));
+  } else if (array.type === 'list') {
+    const list = contentDiv.appendChild(el('ul'));
+    for(let i = 0; i<array.data.length; i+=1) {
+      list.appendChild(el('li',`${array.data[i]}`));
+    }
+  } else if (array.type === 'code') {
+    contentDiv.appendChild(el('code',`${array.data}`));
+  }
+}
 
 function createContent(data, newSlug){
     console.log(data);
@@ -17,9 +46,14 @@ function createContent(data, newSlug){
         image, thumbnail, content,
       }] = data.lectures[i];
     const header = document.querySelector('.header');
+    header.style.backgroundImage = `${image}`;
     header.querySelector('header__category').appendChild(el('p',category));
     header.querySelector('header__title').appendChild(el('p',title));
     const contentDiv = document.querySelector('.content');
+    for(let i = 0; i<content.length; i += 1) {
+
+    }
+
 }
 
 function fetchData(slug) {
